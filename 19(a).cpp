c3a191f957c659ffe9d81947cb53c19771fe6cc7
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 11:21:38 +0600

    19(a).cpp

---
+++ 19(a).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+double a,b;
+cout<< "Enter the price for 1kg of candies: ";
+cin>>d;
+   cout<< "Enter the price for 1 kg of crakers: ";
+   cin>>f;
+    cout<<((3*d)/10)+((4*f)/10)<<endl;
+    }
