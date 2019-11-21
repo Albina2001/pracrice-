920a7657a1894223891f14ff052abeb8b47c744d
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:52:08 +0600

    92.cpp

---
+++ 92.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double answer=0,i,d;
+    d=100;
+    for (i=1;i<=d;i++){
+        answer+=i;
+    }
+    cout<<answer;
+    return 0;
+}
