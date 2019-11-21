2ceb7a75a5da96c7ddf839d5879234f6278efecf
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:49:28 +0600

    86.cpp

---
+++ 86.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int d,answer=0;
+    cin>>d;
+    for (int i=1;i<=n;i++){
+        answer+=i;
+    }
+    cout<<answer;
+    return 0;
+}
