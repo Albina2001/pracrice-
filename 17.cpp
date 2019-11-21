8bb15a7f0d4aef4baa3a633046eb393dba02da30
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 11:15:15 +0600

    17.cpp

---
+++ 17.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Enter three numbers: "
+    double d,f,e;
+    cin>>d>>f>>e;
+    cout<<(d+f+e)/3<<endl;
+    cout<<2*(d+e)-3*f;
+}
