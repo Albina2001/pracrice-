f8279ce2501db245517d475da3bf9841c52b47aa
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:40:39 +0600

    64.cpp

---
+++ 64.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int d,f,e,g,h;
+	cin>>d;
+	f=(d/1000);
+	e=(d/100)%10;
+	g=(d/10)%10;
+	h=(d%10);
+	if ((f==h)&&(f==g))
+	    cout<<"yes";
+	else
+	    cout<<"no";
+	return 0;
+}
