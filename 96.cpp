eaf3906b78c9ca360a0f64b2e8846aa6ecfb07de
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:54:37 +0600

    96.cpp

---
+++ 96.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double p=0,i,d,f;
+    cin>>d>>f;
+    for (i=1;i<=f;i++){
+        p+=(1/(cos(pow(x,i))));
+    }
+    cout<<p;
+    return 0;
+}
