8e46d6296da99d4b0577013c8c813ef4a4eb43df
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:04:19 +0600

    50.cpp

---
+++ 50.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double d,f,e;
+    cin>>d>>f>>e;
+    if ((d==f)||(d==e)||(f==e))
+        cout<<"yes";
+    else 
+        cout<<"no";
+}
