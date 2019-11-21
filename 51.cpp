7ffc628997a3619e26673a845d799a6aa1762f30
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 12:23:09 +0600

    51.cpp

---
+++ 51.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int d,f,e;
+    cin>>d>>f>>e;
+    if (d+f==e || d+e==b || f+e==a)
+        cout << "yes";
+    else cout<< "no";
+    return 0;
+}
