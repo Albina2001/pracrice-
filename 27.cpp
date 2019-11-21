bca451f118baee7a5ea8f4f6a7abb8100e97a686
Albina2001 <57993462+Albina2001@users.noreply.github.com>
2019-11-21 11:35:07 +0600

    27.cpp

---
+++ 27.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int d,f,e,g,h,j;
+    cin>>d>>f>>e;
+    g=d+f;
+    h=e-d;
+    j=d+f+e;
+    d=g;
+    f=h;
+    e=j;
+    cout<<d<<" "<<f<<" "<<e;
+}
